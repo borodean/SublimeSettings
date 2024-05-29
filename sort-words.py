80beab737e31b2ce31e3ed1ba659521c4b4c4e33
Vadym Borodin <borodean@gmail.com>
2024-05-29 05:46:02 +0300

    Improve the sort words script

--- sort-words.py
+++ sort-words.py
@@ -1,7 +1,4 @@
-import sublime
 import sublime_plugin
-
-delim = ' '
 
 class SortWordsCommand(sublime_plugin.TextCommand):
   def run(self, edit):
@@ -9,5 +6,16 @@ class SortWordsCommand(sublime_plugin.TextCommand):
     sel = view.sel()
     for region in sel:
       content = view.substr(region)
-      newContent = delim.join(sorted(content.split(delim)))
-      view.replace(edit, region, newContent)
+      view.replace(edit, region, sort_words(content))
+
+def detect_separator(input: str):
+  for separator in [", ", "; ", " "]:
+    if separator in input:
+      return separator
+  raise ValueError("No valid separator found in the input.")
+
+def sort_words(input: str):
+  separator = detect_separator(input)
+  words = input.split(separator)
+  sorted_words = sorted(words)
+  return separator.join(sorted_words)
